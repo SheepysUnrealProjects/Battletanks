dfcbfd6c7918919fe06059b3e22696896efa7a91
DESKTOP-5CUBI1D\Sheepy <Sheepy@gmx.net>
2018-01-03 13:29:23 +0100

    Better tank movement with OnHit()

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,13 +4,52 @@
 
 
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	DriveTrack();
+	ApplySidewaysForce();
+	CurrentThrottle = 0;
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	
+}
 
-	//TODO clamp actual throttle value so player cant over-drive
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+void UTankTrack::ApplySidewaysForce()
+{
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
+	//Calculate the slippage speed
+	auto Slippagespeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	//Work-out the required acceleration this frame to correct
+	auto CorrectionAcceleration = -Slippagespeed / DeltaTime * GetRightVector();
+
+	//Calculate and apply sideways (F = m a)
+	auto TankRootComponent = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto Correctionforce = (TankRootComponent->GetMass() * CorrectionAcceleration) / 2; //Two tracks
+	TankRootComponent->AddForce(Correctionforce);
+}
+
+void UTankTrack::DriveTrack()
+{
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.7
 
 #pragma once
 
@@ -21,4 +21,17 @@ public:
 	//Max force per track
 	UPROPERTY(EditDefaultsOnly)
 		float TrackMaxDrivingForce = 400000; //Assume 40,000kg tank, and 1g accelleration
+private:
+	UTankTrack();
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	void ApplySidewaysForce();
+
+	void BeginPlay() override;
+	UFUNCTION(BlueprintCallable, Category = "Onhit")
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	void DriveTrack();
+
+	float CurrentThrottle = 0;
 };
